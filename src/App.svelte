ec1c4cc6e3af730e604d9e75a3dddb962b75e651
John Gribbin <johnggribbin@gmail.com>
2020-04-25 22:46:27 -0400

    Add notes and drum samples

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,17 @@
   $: Tone.Transport.bpm.value = bpm;
 
   // sequences - default is four on floor
+  // update to an arry of arrays, first index is row, second index would be the "beat"
+  // Could be an array or arrays, with each item an object
+  // const sequence = [
+  // 	[{checked: true, active: false}, ...], // kick
+  // 		[{checked: true, active: false}, ...], // snare
+  // 			[{checked: true, active: false}, ...], // closed hat
+  // 				[{checked: true, active: false}, ...], // open hat
+  // ]
+
+  // each "row" can be an instrument, that will contain as many "beats" as the length of the sequence it receives
+  // <Instrument sample='./drum' sequence='./row_its_concerned_with' updater = {(beatIndex, isChecked) => sequence[rowIndex][beat].checked = isChecked} />
   const sequences = {
     one: {
       kick: [
@@ -176,7 +187,7 @@
   ];
 
   // assign instruments
-  drums[0].oscillator.type = "triangle";
+  drums[0].oscillator.type = "triangle"; //
   drums[1].oscillator.type = "sine";
   drums[2].oscillator.type = "sawtooth";
   drums[3].oscillator.type = "square";
@@ -189,7 +200,6 @@
   // get all rows
   const rows = document.getElementsByTagName("div");
   // notes that will be played by the insruments
-  const notes = ["G5", "E4", "C3", "A2"];
 
   let index = 0;
 
@@ -202,11 +212,13 @@
     beatCount = step;
     // Map through the 4 rows (instruments)
     for (let i = 0; i < rows.length; i++) {
+      // index will be used to trigger the drum
+      // looping over the sequence as opposed to rows!
       let drum = drums[i];
-      let note = notes[i];
+
       let row = rows[i];
       let input = row.querySelector(`input:nth-child(${step})`);
-      if (input.checked) drum.triggerAttackRelease(note, "16n", time);
+      if (input.checked) drum.triggerAttackRelease("C4", "16n", time);
     }
     index++;
   }
@@ -217,6 +229,26 @@
 
   const stopLoop = () => {
     Tone.Transport.stop();
+  };
+
+  const setSequence = selectedSequence => {
+    Object.keys(sequences).map(function(key, index) {
+      console.log(sequences);
+
+      if (key === selectedSequence) {
+        return { ...sequences.key, selected: true };
+      } else {
+        return { ...sequences.key, selected: false };
+      }
+    });
+
+    console.log(sequences);
+  };
+
+  const kick = new Tone.Player("./samples/808_kick.wav").toMaster();
+
+  const hitKick = () => {
+    kick.start();
   };
 </script>
 
@@ -278,6 +310,12 @@
 <main>
   <h1>808 Sequencer</h1>
   <section>
+
+    <!-- {#each sequence as instrument, rowIndex}
+				<Instrument sample='./drum' sequence='./row_its_concerned_with' label='Kick' updater = {(beatIndex, isChecked) => sequence[rowIndex][beat].checked = isChecked} />
+		
+		{/each} -->
+
     <h2>KICK</h2>
     <div>
       {#each sequences.one.kick as beat, i}
@@ -326,7 +364,20 @@
 
   <button on:click={playLoop}>Play Loop</button>
   <button on:click={stopLoop}>Stop Loop</button>
+  <button on:click={setSequence('clear')}>Clear</button>
+  <button on:click={hitKick}>Hit Kick</button>
 </main>
 
 <!-- TODO
-- Look into syncing Tone.Transport with Tone.Draw  -->
+- Set up global state for the UI and triggering the sounds
+- Add numbers of steps to top
+- Look into syncing Tone.Transport with Tone.Draw  
+- Add some effects like reverb
+- Add option to change the sequence
+- Add button to clear the sequence
+- Update tempo with a svelte slider 
+- Add a curve on tempo updates
+- Add a possible arpeggiator 
+- Break out rows and beats
+- Breaking down the global stat
+-->
